795cbd18797b0f9a491c9212bcf2c254f6117d7c
Shreya <20BCS209@iiitdmj.ac.in>
2025-08-24 23:20:56 +0530

    updates on day 13

--- Vault/src/Vault.sol
+++ Vault/src/Vault.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin\contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin\contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vault is ReentrancyGuard, Pausable, Ownable {
