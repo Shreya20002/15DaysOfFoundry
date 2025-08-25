31d9b13a26082494ba3982f53e494bee7eb579b0
Shreya <20BCS209@iiitdmj.ac.in>
2025-08-25 17:58:46 +0530

    Day 11 cicd update workflows final

---
+++ CICD/src/SimpleBank.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SimpleBank {
+    mapping(address => uint256) public balances;
+    uint256 public totalDeposits;
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount should be greater than zero!");
+        balances[msg.sender] += msg.value; 
+        totalDeposits += msg.value;
+    }
+
+
+    function withdraw(uint256 _amount) public {
+        require(_amount > 0, "Must withdraw something");
+        require(balances[msg.sender] >= _amount, "Not enough balance!");
+
+        balances[msg.sender] -= _amount;
+        totalDeposits -= _amount;
+
+        (bool success,) = msg.sender.call{value: _amount}("");
+        require(success, "Transfer failed");
+    }
+
+    function getBalance(address _user) public view returns (uint256) {
+        return balances[_user];
+    }
+}
---
+++ CICD/test/SimpleBank.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {SimpleBank} from "../src/SimpleBank.sol";
+
+contract SimpleBankTest is Test {
+    SimpleBank public bank;
+
+    function setUp() public {
+        bank = new SimpleBank();
+    }
+
+    function test_deposit() public {
+        bank.deposit{value: 100000}();
+    }
+}
+
